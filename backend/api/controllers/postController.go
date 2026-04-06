fbd45ad561c7e9202b0c24f8665451fee8c9112a
cm748w <213721483@qq.com>
2026-04-06 16:59:28 +0800

    创建post&获取post

---
+++ backend/api/controllers/postController.go
@@ -0,0 +1,123 @@
+package controllers
+
+import (
+	"Server/database"
+	"Server/models"
+	"context"
+	"time"
+
+	"github.com/gofiber/fiber/v2"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+// Create Post
+// @Summary create a new post
+// @Description create new post
+// @Tags Posts
+// @Accept json
+// @Produce json
+// @Param post body models.CreateOrUpdatePost true "post create details"
+// @Success 201 {object} models.PostModel
+// @Failure 400 {object} map[string]interface{}
+// @Security BearerAuth
+// Router /posts [post]
+func CraetePost(c *fiber.Ctx) error {
+
+	var UserSchema = database.DB.Collection("users")
+	var PostSchema = database.DB.Collection("posts")
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+
+	var body models.CreateOrUpdatePost
+	if err := c.BodyParser(&body); err != nil {
+		return c.Status(fiber.StatusBadGateway).JSON(fiber.Map{
+			"error":   "Invalid request body",
+			"details": err.Error(),
+		})
+	}
+
+	// start set data
+	var post models.PostModel
+	post.Creator = c.Locals("userId").(string)
+	post.Likes = make([]string, 0)
+	post.Comments = make([]string, 0)
+	post.CreatedAt = time.Now()
+	post.Title = body.Title
+	post.Message = body.Message
+	post.SelectedFile = body.SelectedFile
+	//
+
+	var user models.UserModel
+	objId, _ := primitive.ObjectIDFromHex(c.Locals("userId").(string))
+	err := UserSchema.FindOne(ctx, bson.M{"_id": objId}).Decode(&user)
+	if err != nil {
+		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
+			"error": err.Error(),
+		})
+	}
+	//
+	post.Name = user.Name
+	// set data end
+	// create post
+	result, err := PostSchema.InsertOne(ctx, &post)
+
+	if err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(err)
+	} else {
+		var createdPost *models.PostModel
+		query := bson.M{"_id": result.InsertedID}
+
+		PostSchema.FindOne(ctx, query).Decode(&createdPost)
+		return c.Status(fiber.StatusCreated).JSON(createdPost)
+	}
+
+}
+
+// Get Post
+// @Summary Get  a new post
+// @Description Get a new post
+// @Tags Posts
+// @Accept json
+// @Produce json
+// @Param id pass string true "Post id"
+// @Success 200 {object} models.PostModel
+// @Failure 400 {object} map[string]interface{}
+// Router /posts/{id} [get]
+func GetPost(c *fiber.Ctx) error {
+
+	var PostSchema = database.DB.Collection("posts")
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+
+	id := c.Params("id")
+	if id == "" {
+		return c.Status(fiber.StatusBadGateway).JSON(fiber.Map{
+			"error": "post id is required",
+		})
+	}
+
+	objID, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		return c.Status(fiber.StatusBadGateway).JSON(fiber.Map{
+			"error": err.Error(),
+		})
+	}
+	var post *models.PostModel
+	query := bson.M{"_id": objID}
+
+	err = PostSchema.FindOne(ctx, query).Decode(&post)
+	if err != nil {
+		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
+			"success": false,
+			"message": "post Not Found",
+			"error":   err.Error(),
+		})
+	}
+
+	return c.Status(fiber.StatusOK).JSON(
+		fiber.Map{
+			"post": post,
+		})
+
+}
