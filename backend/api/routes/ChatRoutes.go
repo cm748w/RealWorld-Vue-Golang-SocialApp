dbfdde29817fb7328808dc81b62694fe56890b0b
cm748w <213721483@qq.com>
2026-04-09 17:34:10 +0800

    鉴权优化

--- backend/api/routes/ChatRoutes.go
+++ backend/api/routes/ChatRoutes.go
@@ -2,11 +2,12 @@ package routes
 
 import (
 	"Server/controllers"
+	"Server/middleware"
 
 	"github.com/gofiber/fiber/v2"
 )
 
 func SetupChatRoutes(app *fiber.App) {
-	app.Post("/chat/sendmessage", controllers.SendMessage)
-
+	app.Post("/chat/sendmessage", middleware.AuthMiddleware, controllers.SendMessage)
+	app.Get("/chat/getmsgsbynums", middleware.AuthMiddleware, controllers.GetMsgsByNums)
 }
