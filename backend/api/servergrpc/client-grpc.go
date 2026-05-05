161e6a0b25ee341c378f397ecfcb3675f07cd980
cm748w <213721483@qq.com>
2026-05-05 21:48:30 +0800

    frontend-realtime-chat-done

---
+++ backend/api/servergrpc/client-grpc.go
@@ -0,0 +1,83 @@
+package servergrpc
+
+import (
+	"Server/models"
+	"Server/protos"
+	"context"
+	"log"
+	"time"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/protobuf/types/known/timestamppb"
+)
+
+type Client struct {
+	conn   *grpc.ClientConn
+	client protos.NotificationGrpcServiceClient
+}
+
+func NewClient() (*Client, error) {
+	conn, err := grpc.NewClient(":8090", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		return nil, err
+	}
+	return &Client{
+		conn:   conn,
+		client: protos.NewNotificationGrpcServiceClient(conn),
+	}, nil
+}
+
+func (c *Client) Close() {
+	c.conn.Close()
+}
+
+func (c *Client) SendGrpcNotification(ctx context.Context, xId, details, mainUserId, targetId string,
+	isRead bool, createdAt time.Time, userName, userAvatar string) error {
+	// Prepare the request
+	request := &protos.NotificationGrpcRequest{
+		XId:        xId,
+		Details:    details,
+		MainUserId: mainUserId,
+		TargetId:   targetId,
+		IsRead:     isRead,
+		CreatedAt:  &timestamppb.Timestamp{Seconds: createdAt.Unix()},
+		User: &protos.Usergrpc{
+			Name:   userName,
+			Avatar: userAvatar,
+		},
+	}
+
+	// call the grpc client func
+	_, err := c.client.SendGrpcNotification(ctx, request)
+	if err != nil {
+		log.Printf("Failed To send notification : %v", err)
+	}
+	return err
+}
+
+func SendNotification(notification models.Notification) error {
+	client, err := NewClient()
+	if err != nil {
+		log.Printf("Failed to create grpc client %v", err)
+		return err
+	}
+	defer client.Close()
+
+	ctx := context.Background()
+	err = client.SendGrpcNotification(
+		ctx, notification.ID.Hex(),
+		notification.Details,
+		notification.MainUserID,
+		notification.TargetID,
+		notification.IsRead,
+		notification.CreatedAt,
+		notification.User.Name,
+		notification.User.Avatar,
+	)
+	if err != nil {
+		log.Printf("Failed to send notification : %v", err)
+		return err
+	}
+	return nil
+}
--- frontend/src/views/Auth.vue
+++ frontend/src/views/Auth.vue
@@ -5,23 +5,10 @@
                 <h1 class="text-h6 text-center">登录</h1>
                 <q-card-section>
                     <form @submit.prevent.stop="Login" class="q-gutter-md">
-                        <q-input 
-                            filled 
-                            v-model="Sin_data.email" 
-                            label="邮箱 *" 
-                            hint="请输入邮箱" 
-                            :rules="emailRules"
-                            lazy-rules 
-                        />
-                        <q-input 
-                            filled 
-                            v-model="Sin_data.password" 
-                            type="password" 
-                            label="密码 *"
-                            hint="请输入密码" 
-                            :rules="passwordRules"
-                            lazy-rules 
-                        />
+                        <q-input filled v-model="Sin_data.email" label="邮箱 *" hint="请输入邮箱" :rules="emailRules"
+                            lazy-rules />
+                        <q-input filled v-model="Sin_data.password" type="password" label="密码 *" hint="请输入密码"
+                            :rules="passwordRules" lazy-rules />
                         <div>
                             <q-btn label="登录" type="submit" color="primary" />
                         </div>
@@ -35,39 +22,14 @@
                 <h1 class="text-h6 text-center">注册</h1>
                 <q-card-section>
                     <form @submit.prevent.stop="Register" class="q-gutter-md">
-                        <q-input 
-                            filled 
-                            v-model="Sup_data.firstName" 
-                            label="名字 *" 
-                            hint="请输入名字"
-                            :rules="nameRules"
-                            lazy-rules 
-                        />
-                        <q-input 
-                            filled 
-                            v-model="Sup_data.lastName" 
-                            label="姓氏 *" 
-                            hint="请输入姓氏"
-                            :rules="nameRules"
-                            lazy-rules 
-                        />
-                        <q-input 
-                            filled 
-                            v-model="Sup_data.email" 
-                            label="邮箱 *" 
-                            hint="请输入邮箱"
-                            :rules="emailRules"
-                            lazy-rules 
-                        />
-                        <q-input 
-                            filled 
-                            v-model="Sup_data.password" 
-                            type="password" 
-                            label="密码 *"
-                            hint="密码（至少6个字符）"
-                            :rules="passwordRules"
-                            lazy-rules 
-                        />
+                        <q-input filled v-model="Sup_data.firstName" label="名字 *" hint="请输入名字" :rules="nameRules"
+                            lazy-rules />
+                        <q-input filled v-model="Sup_data.lastName" label="姓氏 *" hint="请输入姓氏" :rules="nameRules"
+                            lazy-rules />
+                        <q-input filled v-model="Sup_data.email" label="邮箱 *" hint="请输入邮箱" :rules="emailRules"
+                            lazy-rules />
+                        <q-input filled v-model="Sup_data.password" type="password" label="密码 *" hint="密码（至少6个字符）"
+                            :rules="passwordRules" lazy-rules />
                         <div>
                             <q-btn label="注册" type="submit" color="positive" />
                         </div>
@@ -114,35 +76,35 @@ export default {
     },
     methods: {
         ...mapActions('auth', ['signin', 'signup']),
-
+        ...mapActions("RealTimeNotify", ["connectToNotify"]),
         async Login() {
             console.log("login in data", this.Sin_data)
-            
+
             // 手动验证所有字段
             let validate = true;
-            
+
             // 验证邮箱
             for (const rule of this.emailRules) {
                 const error = rule(this.Sin_data.email);
                 if (error !== true) {
                     this.$q.notify({
-                        icon:'eva-alert-circle-outline',
-                        type:'negative',
+                        icon: 'eva-alert-circle-outline',
+                        type: 'negative',
                         message: error
                     });
                     validate = false;
                     break;
                 }
             }
-            
+
             if (validate) {
                 // 验证密码
                 for (const rule of this.passwordRules) {
                     const error = rule(this.Sin_data.password);
                     if (error !== true) {
                         this.$q.notify({
-                            icon:'eva-alert-circle-outline',
-                            type:'negative',
+                            icon: 'eva-alert-circle-outline',
+                            type: 'negative',
                             message: error
                         });
                         validate = false;
@@ -150,73 +112,75 @@ export default {
                     }
                 }
             }
-            
+
             // success and to do next
-            if(validate){
-                var formdata = {email:this.Sin_data.email, password:this.Sin_data.password}
+            if (validate) {
+                var formdata = { email: this.Sin_data.email, password: this.Sin_data.password }
                 const data = await this.signin(formdata);
                 console.log("data response", data)
 
-                if(data?.response?.data?.message || data?.response?.data){
+                if (data?.response?.data?.message || data?.response?.data) {
                     let errorMessage = "登录失败";
                     if (data.response.data.message) {
                         errorMessage = data.response.data.message;
                     } else if (data.response.data) {
                         errorMessage = data.response.data;
                     }
-                    
+
                     // 更详细的错误提示
                     if (errorMessage.includes('invalid email or password')) {
                         this.$q.notify({
-                            icon:'eva-alert-circle-outline',
-                            type:'negative',
-                            message:`邮箱或密码错误，请检查您的凭据。`
+                            icon: 'eva-alert-circle-outline',
+                            type: 'negative',
+                            message: `邮箱或密码错误，请检查您的凭据。`
                         })
                     } else {
                         this.$q.notify({
-                            icon:'eva-alert-circle-outline',
-                            type:'negative',
-                            message:`错误：${errorMessage}`
+                            icon: 'eva-alert-circle-outline',
+                            type: 'negative',
+                            message: `错误：${errorMessage}`
                         })
                     }
                 } else {
                     this.$q.notify({
-                        icon:'eva-alert-circle-outline',
-                        type:'positive',
-                        message:`登录成功，欢迎回来！`
+                        icon: 'eva-alert-circle-outline',
+                        type: 'positive',
+                        message: `登录成功，欢迎回来！`
                     })
+                    this.connectToNotify()
+                    this.$router.push('/')
                 }
             }
         },
         // -----------------------------------------------------------------------------
         async Register() {
             console.log("Register in data", this.Sup_data)
-            
+
             // 手动验证所有字段
             let isValidate = true;
-            
+
             // 验证名字
             for (const rule of this.nameRules) {
                 const error = rule(this.Sup_data.firstName);
                 if (error !== true) {
                     this.$q.notify({
-                        icon:'eva-alert-circle-outline',
-                        type:'negative',
+                        icon: 'eva-alert-circle-outline',
+                        type: 'negative',
                         message: error
                     });
                     isValidate = false;
                     break;
                 }
             }
-            
+
             if (isValidate) {
                 // 验证姓氏
                 for (const rule of this.nameRules) {
                     const error = rule(this.Sup_data.lastName);
                     if (error !== true) {
                         this.$q.notify({
-                            icon:'eva-alert-circle-outline',
-                            type:'negative',
+                            icon: 'eva-alert-circle-outline',
+                            type: 'negative',
                             message: error
                         });
                         isValidate = false;
@@ -224,15 +188,15 @@ export default {
                     }
                 }
             }
-            
+
             if (isValidate) {
                 // 验证邮箱
                 for (const rule of this.emailRules) {
                     const error = rule(this.Sup_data.email);
                     if (error !== true) {
                         this.$q.notify({
-                            icon:'eva-alert-circle-outline',
-                            type:'negative',
+                            icon: 'eva-alert-circle-outline',
+                            type: 'negative',
                             message: error
                         });
                         isValidate = false;
@@ -240,15 +204,15 @@ export default {
                     }
                 }
             }
-            
+
             if (isValidate) {
                 // 验证密码
                 for (const rule of this.passwordRules) {
                     const error = rule(this.Sup_data.password);
                     if (error !== true) {
                         this.$q.notify({
-                            icon:'eva-alert-circle-outline',
-                            type:'negative',
+                            icon: 'eva-alert-circle-outline',
+                            type: 'negative',
                             message: error
                         });
                         isValidate = false;
@@ -256,35 +220,36 @@ export default {
                     }
                 }
             }
-            
-            if(isValidate){
+
+            if (isValidate) {
                 const data = await this.signup(this.Sup_data)
                 console.log("data on register", data)
-                
-                if(data?.response?.data?.message){
+
+                if (data?.response?.data?.message) {
                     let errorMessage = data.response.data.message;
-                    
+
                     // 更详细的错误提示
                     if (errorMessage.includes('already exists')) {
                         this.$q.notify({
-                            icon:'eva-alert-circle-outline',
-                            type:'negative',
-                            message:`该邮箱已被注册，请使用其他邮箱或直接登录。`
+                            icon: 'eva-alert-circle-outline',
+                            type: 'negative',
+                            message: `该邮箱已被注册，请使用其他邮箱或直接登录。`
                         })
                     } else {
                         this.$q.notify({
-                            icon:'eva-alert-circle-outline',
-                            type:'negative',
-                            message:`错误：${errorMessage}`
+                            icon: 'eva-alert-circle-outline',
+                            type: 'negative',
+                            message: `错误：${errorMessage}`
                         })
                     }
                 } else {
                     // 意味着成功了
                     this.$q.notify({
-                        icon:'eva-alert-circle-outline',
-                        type:'positive',
-                        message:`注册成功，欢迎加入我们的社区！`
+                        icon: 'eva-alert-circle-outline',
+                        type: 'positive',
+                        message: `注册成功，欢迎加入我们的社区！`
                     })
+                    this.connectToNotify()
                     // this.$router.push('/')
                 }
             }
