b0e6635d7551a779bc9abed3c9abaf18996148f1
cm748w <213721483@qq.com>
2026-05-09 23:34:56 +0800

    section done !

--- backend/realTimeChat/servegrpc/client-grpc.go
+++ backend/realTimeChat/servegrpc/client-grpc.go
@@ -10,7 +10,9 @@ import (
 )
 
 func GetFollowingFollowersClient(id string) ([]*protos.UserIDsList, error) {
-	conn, err := grpc.NewClient(":5001", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	// conn, err := grpc.NewClient(":5001", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	// DevOps Docker Compose
+	conn, err := grpc.NewClient("GolangApiServer:5001", grpc.WithTransportCredentials(insecure.NewCredentials()))
 
 	if err != nil {
 		log.Fatalf("did not connected:%v", err)
@@ -33,7 +35,9 @@ func GetFollowingFollowersClient(id string) ([]*protos.UserIDsList, error) {
 }
 
 func SendMessageClient(sender, receiver, content string) error {
-	conn, err := grpc.NewClient(":5001", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	// conn, err := grpc.NewClient(":5001", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	// DevOps Docker Compose
+	conn, err := grpc.NewClient("GolangApiServer:5001", grpc.WithTransportCredentials(insecure.NewCredentials()))
 
 	if err != nil {
 		log.Fatalf("did not connected:%v", err)
