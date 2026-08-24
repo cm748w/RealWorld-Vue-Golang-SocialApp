fb528b65b723ac2ec881cd18edc8b790894c4cdc
cm748w <213721483@qq.com>
2026-08-24 22:39:20 +0800

    修复CI/CD

--- frontend/cypress.config.js
+++ frontend/cypress.config.js
@@ -4,7 +4,7 @@ module.exports = defineConfig({
   allowCypressEnv: false,
 
   e2e: {
-    baseUrl: 'http://localhost:80',
+    baseUrl: 'http://localhost:8080',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
